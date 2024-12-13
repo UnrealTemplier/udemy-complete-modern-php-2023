e34edc322fa8791c23819ec9a72eb0e75f2fd64f
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 12:00:38 +0400

    OOP: Creating Namespace.

--- 7. Object-Oriented Programming (OOP)/Account.php
+++ 7. Object-Oriented Programming (OOP)/Account.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace App;
+
 class Account 
 {
     public function __construct(
--- 7. Object-Oriented Programming (OOP)/index.php
+++ 7. Object-Oriented Programming (OOP)/index.php
@@ -4,15 +4,8 @@ declare(strict_types=1);
 
 require_once "Account.php";
 
-$myAccount = null;
+use App\Account;
 
-// The old way
-// if (isset($myAccount)) {
-//     $myAccount->deposit(30)->deposit(50);
-// }
-
-// The new way
-// Null-safe operator
-$myAccount?->deposit(30)->deposit(50);
+$myAccount = new Account("Dean", 100);
 
 var_dump($myAccount);
