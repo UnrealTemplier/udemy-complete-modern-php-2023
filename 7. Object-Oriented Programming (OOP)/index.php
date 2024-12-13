9cdf20a964357e1714a8ae07b59dd1771b08480b
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 12:09:57 +0400

    OOP: Namespaces. More classes.

--- 7. Object-Oriented Programming (OOP)/index.php
+++ 7. Object-Oriented Programming (OOP)/index.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 require_once "App/Account.php";
+require_once "App/SocialMedia.php";
 
 use App\Account;
+use App\SocialMedia;
 
 $myAccount = new Account("Dean", 100);
+$mySocialMedia = new SocialMedia();
 
 var_dump($myAccount);
