f59ce25396c396a70151940971219e0e0192f3ed
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 11:29:30 +0400

    OOP: Null-safe operator.

--- 7. Object-Oriented Programming (OOP)/index.php
+++ 7. Object-Oriented Programming (OOP)/index.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 require_once "Account.php";
 
-$myAccount = new Account("John", 20);
-$myAccount->deposit(30)->deposit(50);
+$myAccount = null;
+
+// The old way
+// if (isset($myAccount)) {
+//     $myAccount->deposit(30)->deposit(50);
+// }
+
+// The new way
+// Null-safe operator
+$myAccount?->deposit(30)->deposit(50);
 
 var_dump($myAccount);
