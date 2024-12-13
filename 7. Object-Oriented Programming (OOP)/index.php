05f4a3278f01f495e054b755561ee1001f149b54
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 12:26:53 +0400

    OOP: Namespaces. Multiple import from the same namespace.

--- 7. Object-Oriented Programming (OOP)/index.php
+++ 7. Object-Oriented Programming (OOP)/index.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 require_once "App/Account.php";
 require_once "App/SocialMedia.php";
 
-use App\Account;
+// instead of using multiple use statements
+// for the same namespace
+// use App\Account;
+// use App\SocialMedia;
+// we can use multiple 'use'
+use App\{Account, SocialMedia};
 
 $myAccount = new Account("Dean", 100);
 
