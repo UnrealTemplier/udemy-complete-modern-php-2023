183a9fd2130b6e1fd1bac3323495bf267ac109b6
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 11:19:24 +0400

    OOP: Properties.

--- 7. Object-Oriented Programming (OOP)/index.php
+++ 7. Object-Oriented Programming (OOP)/index.php
@@ -1,10 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 require_once "Account.php";
 
 $myAccount = new Account;
 $johnsAccount = new Account;
 
-var_dump($myAccount);
+$myAccount->balance = 20;
+
+var_dump($myAccount->balance);
 echo "<br>";
 var_dump($johnsAccount);
