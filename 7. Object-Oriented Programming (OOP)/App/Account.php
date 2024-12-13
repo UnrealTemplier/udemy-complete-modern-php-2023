7dd81b9c1c5cc19236b0be99233963f4a26da342
UnrealTemplier <jaimore.evseev@gmail.com>
2024-12-13 12:24:54 +0400

    OOP: Namespaces. Global namespaces.

--- 7. Object-Oriented Programming (OOP)/App/Account.php
+++ 7. Object-Oriented Programming (OOP)/App/Account.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace App;
 
+// we can import classes from global namespace 
+// with the 'use' keyword
+// without specifying the namespace
+use DateTime;
+$dt = new DateTime();
+
+// or use \ in front of a class name for global namespace
+$dt1 = new \DateTime();
+
+// also we can use aliases with the keyword 'as'
+use DateTime as DT;
+$dt2 = new DT();
+
 class Account 
 {
     public SocialMedia $socialMedia;
